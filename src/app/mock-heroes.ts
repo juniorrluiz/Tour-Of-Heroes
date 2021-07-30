43c6103e23073ed99867948bfb7a76fae89d1cf8
JuniorLuiz <juniorrluiz.contato@gmail.com>
2021-07-30 13:05:25 -0300

    adicionando projeto Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[]=[
+  {id: 11, name: 'Dr Tilambuco'},
+  {id: 12, name: 'Cleitin'},
+  {id: 13, name: 'Nareba'},
+  {id: 14, name: 'Mr Dilsinho'},
+  {id: 15, name: 'Caçador'},
+  {id: 16, name: 'Zé Oureia'},
+  {id: 17, name: 'Miquei mause'},
+  {id: 18, name: 'Tina'},
+  {id: 19, name: 'Cuca'},
+  {id: 20, name: 'Beludo'},
+
+]
